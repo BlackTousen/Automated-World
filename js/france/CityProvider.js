8678d1fdd7624b9602293a9396c4fbe84500bd9b
Aaron Gertler <aaron.gertler@gmail.com>
2020-08-24 11:37:23 -0500

    created necessary js and populated with info

---
+++ js/france/CityProvider.js
@@ -0,0 +1,52 @@
+const cityCollection = [
+    {
+        name: "Paris",
+        area: "40.7",
+        population: "2,148,000",
+        region: "Ile-de-France",
+        topOne: "Eiffel Tower",
+        topTwo: "Louvre Museum",
+        topThree: "Arc de Triomphe"
+    },
+    {
+        name: "Marseille",
+        area: "92.9",
+        population: "861,635",
+        region: "Bouches-du-Rhone",
+        topOne: "Basilique Notre Dame de la Garde",
+        topTwo: "SENSAS Marseille",
+        topThree: "La Cantine de Lynn"
+
+    },
+    {
+        name: "Bordeaux",
+        area: "19.06",
+        population: "249,712",
+        region: "Gironde",
+        topOne: "Bassins de Lumières",
+        topTwo: "BLISSS",
+        topThree: "Porte Quinze"
+    },
+    {
+        name: "Nice",
+        area: "27.77",
+        population: "342,522",
+        region: "French Riviera - Cote d'Azur",
+        topOne: "Old Town",
+        topTwo: "Promenade des Anglais",
+        topThree: "La Route Du Miam"
+    },
+    {
+        name: "Lyon",
+        area: "18.48",
+        population: "513,275",
+        region: "Rhone",
+        topOne: "Basilique Notre Dame de Fourviere",
+        topTwo: "Parc de la Tete d'Or",
+        topThree: "Bouchon Bat D'ARgent"
+    }
+];
+
+export const useCities = () => {
+    return cityCollection.slice()
+};
---
+++ js/france/LandmarkProvider.js
@@ -0,0 +1,41 @@
+const landmarksCollection = [
+    {
+        name: "Eiffel Tower",
+        address: {
+            street: "5 Avenue Anatole France",
+            city: "75007 Paris"
+        }
+    },
+    {
+        name: "Louvre Museum",
+        address: {
+            street: "Rue de Rivoli",
+            city: "75001 Paris"
+        }
+    },
+    {
+        name: "Cathédrale Notre-Dame de Paris",
+        address: {
+            street: "6 Parvis Notre-Dame-Pl.",
+            city: "75004 Paris"
+        }
+    },
+    {
+        name:"Palace of Versailles",
+        address: {
+            street: "Place d'Armes",
+            city: "78000 Versaille"
+        }
+    },
+    {
+        name: "Arc de Triomphe",
+        address: {
+            street: "Place Charles de Gaulle",
+            city: "75008 Paris"
+        }
+    }
+];
+
+export const useLandmarks = () => {
+    return landmarksCollection.slice()
+};
